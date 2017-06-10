b3e445560573dbefa8fe472e4007e4a0f77059c1
badiere.eric <eric.badiere@clickbank.com>
2017-06-09 19:57:34 -0600

    Added more tests.

--- task3/source/mymath-spec.js
+++ task3/source/mymath-spec.js
@@ -10,6 +10,15 @@ describe('MyMath', () => {
        expect(sum).toBe(5);
   });
 
+  it('should add two numbers one being negative', () => {
+      
+      let my = new MyMath();
+
+      let sum = my.add(-1,4);
+
+       expect(sum).toBe(3);
+  });
+
   it('should subtract two numbers', () => {
 
       let my = new MyMath();
@@ -19,6 +28,15 @@ describe('MyMath', () => {
        expect(sum).toBe(3);
   });
 
+   it('should subtract two negative numbers', () => {
+
+      let my = new MyMath();
+
+      let sum = my.subtract(-4,-1);
+
+       expect(sum).toBe(-3);
+  });
+
   it('should multiply two numbers', () => {
 
       let my = new MyMath();
@@ -26,6 +44,15 @@ describe('MyMath', () => {
       let sum = my.multiply(4,2);
 
        expect(sum).toBe(8);
+  });
+
+    it('should multiply two decimal numbers', () => {
+
+      let my = new MyMath();
+
+      let sum = my.multiply(4.5,2.5);
+
+       expect(sum).toBe(11.25);
   });
 
   it('should divide two numbers', () => {
