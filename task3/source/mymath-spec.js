9a0048d35df6104abb9bbfefe2c093564c846afa
badiere.eric <eric.badiere@clickbank.com>
2017-06-09 19:19:30 -0600

    Initial commit of task3.  First run with failing tests.  Commit to allow diffs.

---
+++ task3/source/mymath-spec.js
@@ -0,0 +1,40 @@
+import {MyMath} from './mymath';
+describe('MyMath', () => {
+
+  it('should add two numbers', () => {
+      
+      let my = new MyMath();
+
+      let sum = my.add(1,4);
+
+       expect(sum).toBe(5);
+  });
+
+  it('should subtract two numbers', () => {
+
+      let my = new MyMath();
+
+      let sum = my.subtract(4,1);
+
+       expect(sum).toBe(3);
+  });
+
+  it('should multiply two numbers', () => {
+
+      let my = new MyMath();
+
+      let sum = my.multiply(4,2);
+
+       expect(sum).toBe(8);
+  });
+
+  it('should divide two numbers', () => {
+
+      let my = new MyMath();
+
+      let sum = my.divide(4,2);
+
+       expect(sum).toBe(2);
+  });
+
+});
