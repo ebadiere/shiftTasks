2493cb83b390bfdea1a8be456dd34483a9df1457
badiere.eric <eric.badiere@clickbank.com>
2017-06-13 16:23:24 -0600

    Added validation of the response body.

--- task1/src/test/java/shiftTasks/ShiftTest.java
+++ task1/src/test/java/shiftTasks/ShiftTest.java
@@ -3,6 +3,7 @@ package shiftTasks;
 import org.junit.Test;
 
 import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 /**
  * Created by badiere.eric on 6/13/17.
@@ -10,13 +11,14 @@ import static com.jayway.restassured.RestAssured.given;
 public class ShiftTest {
     @Test
     public void testFailingPost(){
-        String examplePost = "{\"withdrawal\":\"AAAAAAAAAAAAA\", \"pair\":\"btc_ltc\", returnAddress:\"BBBBBBBBBBB\"}";
+        String examplePost = "{\"withdrawal\":\"AAAAAAAAAAAAA\", \"pair\":\"btc_ltc\", \"returnAddress\":\"BBBBBBBBBBB\"}";
 
         given()
+            .accept("application/json")
             .contentType("application/json")
             .body(examplePost)
             .when().post("https://shapeshift.io/shift").then()
-            .statusCode(200);
+            .statusCode(200).body("error", equalTo("Warning: Return address appears to be invalid for the deposit coin type."));
 
 
     }
